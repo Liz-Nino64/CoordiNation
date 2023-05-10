f28cf4f5632468b57a2410c0c65f48ae63c360e0
Michael Muniz <m.s.muniz@sbcglobal.net>
2023-05-10 00:56:23 -0500

    Cleaned up code

--- controllers/api/taskRoutes.js
+++ controllers/api/taskRoutes.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const { Task, User } = require('../../models');
 const withAuth = require('../../utils/auth');
-const sequelize = require('../../config/connection');
 
 router.post('update/:id', withAuth, async (req, res) => {
   try {
@@ -36,11 +35,9 @@ router.delete('/delete/:id', withAuth, async (req, res) => {
   }
 });
 
-
 router.get('/find/:id', async (req, res) => {
   try {
     const taskData = await Task.findByPk(req.params.id, {
-
       include: [
         {
           model: User,
@@ -60,42 +57,5 @@ router.get('/find/:id', async (req, res) => {
     res.status(500).json(err);
   }
 });
-
-// router.get('/find', async (req, res) => {
-//   try {
-//     const taskData = await Task.findAll({
-//       where:{
-//         user_id: req.session.user_id
-//       },
-//       attributes: ['id', 'taskname', 'description', 'status', 'priority', 'dateDue', 'user_id', [sequelize.literal('user.name'), 'userName']],
-//       include: [
-//         {
-//           model: User,
-//           attributes: ['name'],
-//           raw:true
-//         }
-//       ],
-//       raw:true
-//     });
-//     console.log(taskData);
-//     //const task = taskData.get({ raw: true });
-//     res.render('task', {
-//       taskData,
-//       logged_in: req.session.logged_in,
-//     });
-//     /* console.log(task);
-//     res.render('task', {
-//       ...task,
-//       logged_in: req.session.logged_in,
-//     }); */
-
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
-
-
-
 
 module.exports = router;
